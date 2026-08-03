b294ea54634efe1a19148d31f817f93023a85f86
DWGPPG <narakorn.pa@primepower.co.th>
2026-08-03 08:45:40 +0700

    Delete assets directory

--- assets/js/services/project-service.js
+++
@@ -1,9 +0,0 @@
-import { state } from "../state.js";
-
-export function getActiveProjects() {
-  return (state.masterData.projects || []).filter((project) => !project.IsHidden);
-}
-
-export function findProjectByName(name) {
-  return getActiveProjects().find((project) => project.Title === name);
-}
